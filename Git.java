38d75fcae7db9d218af81afb691451f5785bcab3
Aiden <ayasharian1@hwemail.com>
2025-09-24 12:57:03 -0700

    (GP-2.1): Program Can Initialize Repository
    Wrote a method that creates a git folder containing an objects folder, a HEAD file, and an index file.

--- Git.java
+++ Git.java
@@ -1,5 +1,25 @@
+import java.io.*;
+import java.nio.file.*;
+
 public class Git {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        createGitRepository();
     }
+    
+    public static boolean createGitRepository() throws IOException {
+        File git = new File("git");
+        if (!git.exists()) {
+            git.mkdir();
+            File objects = new File("git/objects");
+            objects.mkdir();
+            File index = new File("git/index");
+            index.createNewFile();
+            File head = new File("git/HEAD");
+            head.createNewFile();
+            System.out.println("Git Repository Created");
+            return true;
+        }
+        System.out.println("Git Repository Already Exists");
+        return false;
+    } 
 }
