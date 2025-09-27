a3f822861aba710578e6e8cb68a6e7cd7bf49df3
Aiden <ayasharian1@hwemail.com>
2025-09-27 13:10:05 -0700

    Completed 2.4
    Added index file functionality. Question: How does index work with 2 txt files that share a hash?

--- StretchTester.java
+++ StretchTester.java
@@ -9,10 +9,15 @@ public class StretchTester {
         // System.out.println(Git.createGitRepository());
         // System.out.println(verify());
         // System.out.println(cleanup());
-        System.out.println(Git.createBlob("Priscilla.txt"));
-        System.out.println(Git.createBlob("Aiden.txt"));
-        System.out.println(Git.createBlob("AlsoPriscilla.txt"));
-        System.out.println(resetAidenPriscillaBlob());
+        // System.out.println(Git.createBlob("Priscilla.txt"));
+        // System.out.println(Git.createBlob("Aiden.txt"));
+        // System.out.println(Git.createBlob("AlsoPriscilla.txt"));
+        // System.out.println(readIndex());
+        // deleteIndex();
+        // System.out.println(readIndex());
+        // System.out.println(resetAidenPriscillaBlob());
+        System.out.println(Git.fullReset());
+
     }
 
     public static boolean verify() {
@@ -35,7 +40,7 @@ public class StretchTester {
         return true;
     }
 
-    public static boolean cleanup() throws IOException { //swithing to nio for the future
+    public static boolean cleanup() throws IOException { // swithing to nio for the future
         Path current = Path.of("git");
         if (Files.isDirectory(current)) {
             deleteHelper(current);
@@ -45,7 +50,7 @@ public class StretchTester {
         return false;
     }
 
-    public static void deleteHelper(Path path) throws IOException {//docs.oracle.com/javase/8/docs/api/java/util/stream/Stream.html
+    public static void deleteHelper(Path path) throws IOException {// docs.oracle.com/javase/8/docs/api/java/util/stream/Stream.html
         Stream<Path> stream = Files.walk(path);
         boolean isFirst = true;
         for (Path p : (Iterable<Path>) stream::iterator) {
@@ -61,25 +66,35 @@ public class StretchTester {
         stream.close();
     }
 
-
     public static boolean checkIfBlobExists(String hash) {
         Path p = Path.of("git/objects/" + hash);
         return Files.isRegularFile(p);
     }
 
     public static boolean resetAidenPriscillaBlob() throws IOException {
-        if (checkIfBlobExists("f07609351393d43cb2ce86b263720721c69e4d15")) { //priscilla
+        if (checkIfBlobExists("f07609351393d43cb2ce86b263720721c69e4d15")) { // priscilla
             Path p = Path.of("git/objects/f07609351393d43cb2ce86b263720721c69e4d15");
             Files.delete(p);
         } else {
             return false;
         }
-        if (checkIfBlobExists("1faf1e8390cdac9204f160c35828cc423b7acd7b")) { //aiden
-             Path p = Path.of("git/objects/1faf1e8390cdac9204f160c35828cc423b7acd7b");
+        if (checkIfBlobExists("1faf1e8390cdac9204f160c35828cc423b7acd7b")) { // aiden
+            Path p = Path.of("git/objects/1faf1e8390cdac9204f160c35828cc423b7acd7b");
             Files.delete(p);
         } else {
             return false;
         }
+        return true;
+    }
+
+    public static String readIndex() throws IOException {
+        Path p = Path.of("git/index");
+        return (Files.readString(p));
+    }
+
+    public static boolean deleteIndex() throws IOException {
+        Path p = Path.of("git/index");
+        Files.writeString(p, "");
         return true;
     }
 }
